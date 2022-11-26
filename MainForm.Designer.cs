69cec6c56758bd2fdd6504c881ee14c93762b241
HelloQ <znznznzmzn@naver.com>
2022-11-26 17:12:00 +0900

    초기 커밋.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace HelloPixelizer {
+    partial class MainForm {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Original_Picture = new System.Windows.Forms.PictureBox();
+            this.Converted_Picture = new System.Windows.Forms.PictureBox();
+            this.ConversionButton = new System.Windows.Forms.Button();
+            this.PixelSizeController = new System.Windows.Forms.NumericUpDown();
+            this.Pixel_Size_Label = new System.Windows.Forms.Label();
+            this.Original_label = new System.Windows.Forms.Label();
+            this.Converted_Label = new System.Windows.Forms.Label();
+            this.colorPicker = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.Label_OpenLink = new System.Windows.Forms.Label();
+            this.Label_SaveLink = new System.Windows.Forms.Label();
+            this.PalleteLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.LoadPalleteButton = new System.Windows.Forms.Button();
+            this.openPalleteDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.PalleteClearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Original_Picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Converted_Picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PixelSizeController)).BeginInit();
+            this.PalleteLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Original_Picture
+            // 
+            this.Original_Picture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Original_Picture.BackgroundImage")));
+            this.Original_Picture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Original_Picture.Location = new System.Drawing.Point(12, 38);
+            this.Original_Picture.Name = "Original_Picture";
+            this.Original_Picture.Size = new System.Drawing.Size(350, 350);
+            this.Original_Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Original_Picture.TabIndex = 0;
+            this.Original_Picture.TabStop = false;
+            this.Original_Picture.Click += new System.EventHandler(this.Original_Picture_Click);
+            this.Original_Picture.DragDrop += new System.Windows.Forms.DragEventHandler(this.Original_Picture_DragDrop);
+            this.Original_Picture.DragEnter += new System.Windows.Forms.DragEventHandler(this.Original_Picture_DragEnter);
+            this.Original_Picture.Paint += new System.Windows.Forms.PaintEventHandler(this.Original_Picture_Paint);
+            // 
+            // Converted_Picture
+            // 
+            this.Converted_Picture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Converted_Picture.BackgroundImage")));
+            this.Converted_Picture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Converted_Picture.Location = new System.Drawing.Point(472, 38);
+            this.Converted_Picture.Name = "Converted_Picture";
+            this.Converted_Picture.Size = new System.Drawing.Size(350, 350);
+            this.Converted_Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Converted_Picture.TabIndex = 1;
+            this.Converted_Picture.TabStop = false;
+            this.Converted_Picture.Click += new System.EventHandler(this.Converted_Picture_Click);
+            this.Converted_Picture.Paint += new System.Windows.Forms.PaintEventHandler(this.Converted_Picture_Paint);
+            // 
+            // ConversionButton
+            // 
+            this.ConversionButton.Location = new System.Drawing.Point(385, 213);
+            this.ConversionButton.Name = "ConversionButton";
+            this.ConversionButton.Size = new System.Drawing.Size(66, 32);
+            this.ConversionButton.TabIndex = 2;
+            this.ConversionButton.Text = "변환";
+            this.ConversionButton.UseVisualStyleBackColor = true;
+            this.ConversionButton.Click += new System.EventHandler(this.ConversionButton_Click);
+            // 
+            // PixelSizeController
+            // 
+            this.PixelSizeController.Location = new System.Drawing.Point(427, 252);
+            this.PixelSizeController.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.PixelSizeController.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.PixelSizeController.Name = "PixelSizeController";
+            this.PixelSizeController.Size = new System.Drawing.Size(39, 21);
+            this.PixelSizeController.TabIndex = 3;
+            this.PixelSizeController.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Pixel_Size_Label
+            // 
+            this.Pixel_Size_Label.AutoSize = true;
+            this.Pixel_Size_Label.Location = new System.Drawing.Point(368, 254);
+            this.Pixel_Size_Label.Name = "Pixel_Size_Label";
+            this.Pixel_Size_Label.Size = new System.Drawing.Size(59, 15);
+            this.Pixel_Size_Label.TabIndex = 4;
+            this.Pixel_Size_Label.Text = "Pixel Size";
+            // 
+            // Original_label
+            // 
+            this.Original_label.AutoSize = true;
+            this.Original_label.Location = new System.Drawing.Point(160, 20);
+            this.Original_label.Name = "Original_label";
+            this.Original_label.Size = new System.Drawing.Size(31, 15);
+            this.Original_label.TabIndex = 5;
+            this.Original_label.Text = "원본";
+            // 
+            // Converted_Label
+            // 
+            this.Converted_Label.AutoSize = true;
+            this.Converted_Label.Location = new System.Drawing.Point(628, 20);
+            this.Converted_Label.Name = "Converted_Label";
+            this.Converted_Label.Size = new System.Drawing.Size(31, 15);
+            this.Converted_Label.TabIndex = 6;
+            this.Converted_Label.Text = "변환";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "\"Image files (*.jpg, *.jpeg, *.jpe, *.png, *.jfif) | *.jpg; *.jpeg; *.jpe; *.png;" +
+    " *.jfif\"";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(472, 394);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(68, 27);
+            this.SaveButton.TabIndex = 7;
+            this.SaveButton.Text = "저장";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(12, 394);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(68, 27);
+            this.OpenButton.TabIndex = 8;
+            this.OpenButton.Text = "불러오기";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // Label_OpenLink
+            // 
+            this.Label_OpenLink.AutoSize = true;
+            this.Label_OpenLink.Location = new System.Drawing.Point(86, 400);
+            this.Label_OpenLink.Name = "Label_OpenLink";
+            this.Label_OpenLink.Size = new System.Drawing.Size(61, 15);
+            this.Label_OpenLink.TabIndex = 9;
+            this.Label_OpenLink.Text = "Load Link";
+            // 
+            // Label_SaveLink
+            // 
+            this.Label_SaveLink.AutoSize = true;
+            this.Label_SaveLink.Location = new System.Drawing.Point(546, 400);
+            this.Label_SaveLink.Name = "Label_SaveLink";
+            this.Label_SaveLink.Size = new System.Drawing.Size(60, 15);
+            this.Label_SaveLink.TabIndex = 10;
+            this.Label_SaveLink.Text = "Save Link";
+            // 
+            // PalleteLayout
+            // 
+            this.PalleteLayout.Controls.Add(this.ColorButton);
+            this.PalleteLayout.Location = new System.Drawing.Point(13, 428);
+            this.PalleteLayout.Name = "PalleteLayout";
+            this.PalleteLayout.Size = new System.Drawing.Size(809, 121);
+            this.PalleteLayout.TabIndex = 11;
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.BackColor = System.Drawing.Color.White;
+            this.ColorButton.Location = new System.Drawing.Point(3, 3);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(25, 25);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.Text = "+";
+            this.ColorButton.UseVisualStyleBackColor = false;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // LoadPalleteButton
+            // 
+            this.LoadPalleteButton.Location = new System.Drawing.Point(736, 400);
+            this.LoadPalleteButton.Name = "LoadPalleteButton";
+            this.LoadPalleteButton.Size = new System.Drawing.Size(86, 23);
+            this.LoadPalleteButton.TabIndex = 13;
+            this.LoadPalleteButton.Text = "Load Pallete";
+            this.LoadPalleteButton.UseVisualStyleBackColor = true;
+            this.LoadPalleteButton.Click += new System.EventHandler(this.LoadPalleteButton_Click);
+            // 
+            // openPalleteDialog
+            // 
+            this.openPalleteDialog.Filter = "\"Image files (*.jpg, *.jpeg, *.jpe, *.png, *.jfif) | *.jpg; *.jpeg; *.jpe; *.png;" +
+    " *.jfif\"";
+            this.openPalleteDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openPalleteDialog_FileOk);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "\"png\"";
+            this.saveFileDialog.Filter = "\"Image files (*.jpg, *.jpeg, *.jpe, *.png, *.jfif) | *.jpg; *.jpeg; *.jpe; *.png;" +
+    " *.jfif\"";
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // PalleteClearButton
+            // 
+            this.PalleteClearButton.Location = new System.Drawing.Point(655, 399);
+            this.PalleteClearButton.Name = "PalleteClearButton";
+            this.PalleteClearButton.Size = new System.Drawing.Size(75, 23);
+            this.PalleteClearButton.TabIndex = 14;
+            this.PalleteClearButton.Text = "Clear";
+            this.PalleteClearButton.UseVisualStyleBackColor = true;
+            this.PalleteClearButton.Click += new System.EventHandler(this.PalleteClearButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(834, 561);
+            this.Controls.Add(this.PalleteClearButton);
+            this.Controls.Add(this.LoadPalleteButton);
+            this.Controls.Add(this.PalleteLayout);
+            this.Controls.Add(this.Label_SaveLink);
+            this.Controls.Add(this.Label_OpenLink);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.Converted_Label);
+            this.Controls.Add(this.Original_label);
+            this.Controls.Add(this.Pixel_Size_Label);
+            this.Controls.Add(this.PixelSizeController);
+            this.Controls.Add(this.ConversionButton);
+            this.Controls.Add(this.Converted_Picture);
+            this.Controls.Add(this.Original_Picture);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(850, 535);
+            this.Name = "MainForm";
+            this.Text = "HelloPixelizer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Original_Picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Converted_Picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PixelSizeController)).EndInit();
+            this.PalleteLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ConversionButton;
+        private System.Windows.Forms.NumericUpDown PixelSizeController;
+        private System.Windows.Forms.Label Pixel_Size_Label;
+        private System.Windows.Forms.Label Original_label;
+        private System.Windows.Forms.Label Converted_Label;
+        public System.Windows.Forms.ColorDialog colorPicker;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Label Label_OpenLink;
+        private System.Windows.Forms.Label Label_SaveLink;
+        private System.Windows.Forms.FlowLayoutPanel PalleteLayout;
+        public System.Windows.Forms.PictureBox Original_Picture;
+        private System.Windows.Forms.PictureBox Converted_Picture;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Button LoadPalleteButton;
+        private System.Windows.Forms.OpenFileDialog openPalleteDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button PalleteClearButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+
+namespace HelloPixelizer {
+	public partial class MainForm : Form {
+		public static List<customColorButton> Pallete = new List<customColorButton>();
+		Bitmap Converted_Bitmap;
+		string[] Formats = { "png", "PNG", "JPG", "JPEG", "jpg", "jpeg" };
+		Rectangle PictureBox_Rect = new Rectangle(0, 0, 350, 350);
+
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		//시작시
+		private void MainForm_Load(object sender, EventArgs e) {
+			Original_Picture.AllowDrop = true;
+		}
+
+		//변환버튼
+		private void ConversionButton_Click(object sender, EventArgs e) {
+			if (Label_OpenLink.Text == "Load Link") {
+				MessageBox.Show("선택된 이미지가 없습니다.");
+				return;
+			}
+			Cursor.Current = Cursors.WaitCursor;
+			try {
+				Converted_Bitmap = Pixelate(new Bitmap(Label_OpenLink.Text), Convert.ToInt32(PixelSizeController.Value));
+				Converted_Picture.Image = (Pallete.Count > 0) ? PixelPallete(Converted_Bitmap) : Converted_Bitmap;
+			}
+			catch (ArgumentException) {
+				MessageBox.Show("파일을 확인해 주세요.");
+			}
+			Cursor.Current = Cursors.Default;
+		}
+
+		//로드된 이미지 클릭
+		private void Original_Picture_Click(object sender, EventArgs e) {
+			OpenButton_Click(sender, e);
+		}
+		//로드버튼
+		private void OpenButton_Click(object sender, EventArgs e) {
+			openFileDialog.ShowDialog();
+		}
+		//변환된 이미지 클릭
+		private void Converted_Picture_Click(object sender, EventArgs e) {
+			SaveButton_Click(sender, e);
+		}
+		//저장 버튼
+		private void SaveButton_Click(object sender, EventArgs e) {
+			if (Converted_Picture.Image == null) {
+				MessageBox.Show("변환된 파일이 없습니다.");
+				return;
+			}
+			saveFileDialog.FileName = "Converted.png";
+			saveFileDialog.ShowDialog();
+		}
+
+		//로드
+		private void openFileDialog_FileOk(object sender, CancelEventArgs e) {
+			Label_OpenLink.Text = openFileDialog.FileName;
+			Image target;
+			try {
+				target = Image.FromFile(@Label_OpenLink.Text);
+				if (target.Width >= 5000 || target.Height >= 5000) { 
+					MessageBox.Show("파일이 너무 큽니다. Width 5000, Height 5000 이하의 사진만 가능합니다.");
+					return;
+				}
+				Original_Picture.Image = target;
+			}
+			catch (ArgumentException) {
+				MessageBox.Show("파일을 확인해 주세요.");
+			}
+		}
+		//저장
+		private void saveFileDialog_FileOk(object sender, CancelEventArgs e) {
+			Label_SaveLink.Text = saveFileDialog.FileName;
+			try {
+				Converted_Picture.Image.Save(Label_SaveLink.Text, ImageFormat.Png);
+			}
+			catch (ArgumentException) {
+				MessageBox.Show("파일을 확인해 주세요.");
+			}
+			Label_SaveLink.Text = "저장 완료";
+		}
+
+		//픽셀아트 그리기(원본)
+		private void Original_Picture_Paint(object sender, PaintEventArgs e) {
+			if (Original_Picture.Image == null) return;
+			e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+			e.Graphics.DrawImage(Original_Picture.Image, GetAspectRect(Original_Picture.Image));
+		}
+		//픽셀아트 그리기(편집본)
+		private void Converted_Picture_Paint(object sender, PaintEventArgs e) {
+			if (Converted_Picture.Image == null) return;
+			e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+			e.Graphics.DrawImage(Converted_Picture.Image, GetAspectRect(Original_Picture.Image));
+		}
+		//Rect구하기
+		public Rectangle GetAspectRect(Image target){
+			Rectangle result = PictureBox_Rect;
+			if (target.Width == target.Height) return result;
+
+			if (target.Width > target.Height) {
+				result.Height = (target.Height * result.Width) / target.Width;
+				result.Y = PictureBox_Rect.Height / 2 - (result.Height / 2);
+			}
+			else {
+				result.Width = (target.Width * result.Height) / target.Height;
+				result.X = PictureBox_Rect.Width / 2 - (result.Width / 2);
+			}
+			return result;
+		}
+
+		//픽셀화 변환 작동
+		public Bitmap Pixelate(Bitmap OriginalBitmap, int PixelSize) {
+			if (PixelSize == 1) return OriginalBitmap;
+			if (OriginalBitmap.Width < PixelSize || OriginalBitmap.Height < PixelSize) {
+				MessageBox.Show("픽셀 크기가 원본 크기를 초과합니다.");
+				return OriginalBitmap;
+			}
+
+			Bitmap result = new Bitmap(OriginalBitmap.Width / PixelSize, OriginalBitmap.Height / PixelSize);
+			int r, g, b;
+			Color Target;
+			int sqr = PixelSize * PixelSize;
+			Rectangle Area = new Rectangle();
+
+			for (int y = 0; y < result.Height; y++) {
+				for (int x = 0; x < result.Width; x++) {
+                    r = g = b = 0;
+					Area.Width = Math.Clamp((x + 1) * PixelSize, 0, OriginalBitmap.Width);
+					Area.Height = Math.Clamp((y + 1) * PixelSize, 0, OriginalBitmap.Height);
+					Area.X = x * PixelSize;
+					Area.Y = y * PixelSize;
+					for (int i = Area.Y; i < Area.Height; i++) {
+						for (int j = Area.X; j < Area.Width; j++) {
+								Target = OriginalBitmap.GetPixel(j, i);
+								r += Target.R;
+								g += Target.G;
+								b += Target.B;
+						}
+					}
+					r /= sqr;
+					g /= sqr;
+					b /= sqr;
+					result.SetPixel(x, y, Color.FromArgb(r, g, b));
+				}
+			}
+			return result;
+		}
+		//팔레트 적용
+		public Bitmap PixelPallete(Bitmap OriginalBitmap) {
+			Bitmap result = new Bitmap(OriginalBitmap.Width, OriginalBitmap.Height);
+			float close_dist = 0;
+			float dist = 0;
+			int SPN;
+			for (int x = 0; x < OriginalBitmap.Width; x++) {
+				for (int y = 0; y < OriginalBitmap.Height; y++) {
+					SPN = 0;
+					close_dist = Calc_ColorDist(OriginalBitmap.GetPixel(x, y), Pallete[SPN].BackColor);
+					for (int i = 1; i < Pallete.Count; i++) {
+						dist = Calc_ColorDist(OriginalBitmap.GetPixel(x, y), Pallete[i].BackColor);
+						if (close_dist > dist) {
+							SPN = i;
+							close_dist = dist;
+						}
+					}
+					result.SetPixel(x, y, Pallete[SPN].BackColor);
+				}
+			}
+            return result;
+		}
+		// 색 거리 계산(팔레트 적용을 위한 작업)
+		public float Calc_ColorDist(Color A, Color B) {
+			if (B.A == 0) return 256;
+			float r = B.R - A.R;
+			float g = B.G - A.G;
+			float b = B.B - A.B;
+			float a = B.A - A.A;
+			return (float)Math.Sqrt(r * r + g * g + b * b + a * a);
+		}
+		
+		//팔레트 버튼 클릭
+        private void ColorButton_Click(object sender, EventArgs e) {
+			if(colorPicker.ShowDialog() == DialogResult.OK) {
+					Button btn = new customColorButton(colorPicker.Color);
+					btn.MouseUp += new MouseEventHandler(customColorButton_Click);
+					PalleteLayout.Controls.Add(btn);
+			}
+		}
+		// 생성된 커스텀 팔레트 버튼 클릭
+		public void customColorButton_Click(Object sender, MouseEventArgs e) {
+			if (e.Button == MouseButtons.Left) {
+				if (colorPicker.ShowDialog() == DialogResult.OK)
+					((customColorButton)sender).BackColor = colorPicker.Color;
+			} else if (e.Button == MouseButtons.Right) {
+				Pallete.Remove((customColorButton)sender);
+				PalleteLayout.Controls.Remove((customColorButton)sender);
+			}
+		}
+		//커스텀 팔레트
+		public class customColorButton : Button {
+			public customColorButton(Color Data) {
+				this.Size = new Size(25, 25);
+				this.BackColor = Data;
+				Pallete.Add(this);
+			}
+		}
+		
+		//팔레트 로드
+        private void openPalleteDialog_FileOk(object sender, CancelEventArgs e) {
+            try { 
+				Bitmap data = new Bitmap(Image.FromFile(@openPalleteDialog.FileName.ToString()));
+				Color target;
+				bool jmp;
+				for (int y = 0; y < data.Height; y++) {
+					for (int x = 0; x < data.Width; x++) {
+						target = data.GetPixel(x, y);
+						jmp = false;
+						for (int i = 0; i < Pallete.Count; i++) {
+							if (jmp = (Pallete[i].BackColor == target)) break;
+						}
+						if (jmp) continue;
+						if (Pallete.Count >= 103) {
+							MessageBox.Show("팔레트가 너무 많습니다.");
+							return;
+						}
+						Button btn = new customColorButton(target);
+						btn.MouseUp += new MouseEventHandler(customColorButton_Click);
+						PalleteLayout.Controls.Add(btn);
+					}
+				}
+			} catch (ArgumentException) {
+				MessageBox.Show("파일을 확인해 주세요.");
+			}
+		}
+        private void LoadPalleteButton_Click(object sender, EventArgs e) {
+			openPalleteDialog.ShowDialog();
+		}
+
+		//팔레트 제거
+        private void PalleteClearButton_Click(object sender, EventArgs e) {
+			Pallete.Clear();
+			Button tmp = ColorButton;
+			PalleteLayout.Controls.Clear();
+			PalleteLayout.Controls.Add(tmp);
+		}
+
+        private void Original_Picture_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+		}
+		private void Original_Picture_DragDrop(object sender, DragEventArgs e) {
+			string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+			if (files.Length < 1) return;
+			else if (files.Length > 1) {
+				MessageBox.Show("하나의 파일만 올려 주세요.");
+				return;
+			}
+			if(!CheckFileFormat(files[0], Formats)) return;
+			try {
+				Image data = Image.FromFile(files[0]);
+				if (data.Width >= 5000 || data.Height >= 5000) {
+					MessageBox.Show("파일이 너무 큽니다. Width 5000, Height 5000 이하의 사진만 가능합니다.");
+					return;
+				}
+				Original_Picture.Image = data;
+				Label_OpenLink.Text = files[0];
+			}
+			catch (ArgumentException) {
+				MessageBox.Show("파일을 확인해 주세요.");
+			}
+        }
+		private bool CheckFileFormat(string target, string [] formats) {
+			string target_format = "";
+			for (int i = target.Length - 1; i >= 0; i--) {
+				if (target[i] == '.') {
+					target_format = target.Substring(i + 1, target.Length - (i + 1));
+					break;
+				}
+			}
+			for (int i = 0; i < formats.Length; i++) {
+				if (target_format.Equals(formats[i])) 
+					return true;
+			}
+			return false;
+		}
+	}
+}
